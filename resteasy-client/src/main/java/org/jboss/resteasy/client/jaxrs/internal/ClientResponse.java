bb6661bcd6b77d504d7a9d6e3129c5e702da863e
Alessio Soldano <asoldano@redhat.com>
2017-02-09 22:34:48 +0100

    Merging Jim's SSE impl from his server-sent-event' branch (no tests yet)

--- resteasy-client/src/main/java/org/jboss/resteasy/client/jaxrs/internal/ClientResponse.java
+++ resteasy-client/src/main/java/org/jboss/resteasy/client/jaxrs/internal/ClientResponse.java
@@ -19,6 +19,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Providers;
 import javax.ws.rs.ext.ReaderInterceptor;
+import javax.ws.rs.sse.SseEventInput;
 
 import java.io.ByteArrayInputStream;
 import java.io.FilterInputStream;
@@ -201,7 +202,10 @@ public abstract class ClientResponse extends BuiltResponse
             {
                try
                {
-            	   close();
+                  if (!SseEventInput.class.isInstance(entity))
+                  {
+                     close();
+                  }
                }
                catch (Exception ignored)
                {
